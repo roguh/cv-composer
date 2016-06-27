14b01e787804c39c940fc7c1a4583edae482a697
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-06-26 19:33:11 -0600

    Define and use new algorithm class

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,15 @@ For help on an algorithm:
 Available algorithms:
   convolution
   morphology, erode, dilate
-  histogram_equalization 
+  histogram_equalization
 
-  selective_sharpen 
+  selective_sharpen
 
-  distance_transform 
+  distance_transform
 
   canny
 
-  two_pass 
+  two_pass
   snakes
   level_sets
 
@@ -70,22 +70,24 @@ bool open_camera(int id, VideoCapture& cap)
 
 int main(int argc, char** argv )
 {
+    std::vector<Mat> frames, images;
     std::vector<VideoCapture> cameras;
-    std::vector<Mat> video_frames;
     std::vector<std::string> image_names;
-    std::vector<Mat> images;
 
-    std::map<std::string, std::shared_ptr<Interface>> algorithms;
-    algorithms["canny"] = std::make_shared<CannyInterface>();
-    algorithms["convolution"] = std::make_shared<ConvolutionInterface>();
-    algorithms["two_pass"] = std::make_shared<TwoPassInterface>();
+    /* define available algorithms */
+    std::map<std::string, std::function<std::shared_ptr<FrameAlgorithm>()>> algs;
 
-    std::vector<std::string> v_argv(argc - 1);
-    for (int i = 1; i < argc; i++) {
-        v_argv[i - 1] = std::string(argv[i]) ;
-    }
+    algs["canny"] = []() {
+        return std::make_shared<CannyAlgorithm>(); };
+    algs["convolution"] = []() {
+        return std::make_shared<ConvolutionAlgorithm>(); };
+    algs["two_pass"] = []() {
+        return std::make_shared<TwoPassAlgorithm>(); };
 
-    for (auto& a: algorithms) {
+    std::vector<std::string> v_argv(argv + 1, argv + argc);
+
+    /* add algorithm names to docstring */
+    for (auto& a: algs) {
         doc += "  " + a.first + "\n";
     }
 
@@ -93,22 +95,21 @@ int main(int argc, char** argv )
                          true, // print help and exit
                          "composer 0.0.0", // version
                          true); // leave options at end alone
+    auto main_args = args;
 
     /* open camera */
-    if (args["--camera"].asBool()) {
+    if (main_args["--camera"].asBool()) {
         VideoCapture cap;
         if (open_camera(0, cap)) {
-            Mat mat;
             cameras.push_back(cap);
-            video_frames.push_back(mat);
-            cameras.back() >> video_frames.back();
+            frames.push_back(Mat());
+            cameras.back() >> frames.back();
         }
     }
 
     /* open images */
-    for (auto& fname : args["--image"].asStringList()) {
+    for (auto& fname : main_args["--image"].asStringList()) {
         std::cout << "Reading from '" << fname << "'" << std::endl;
-
         Mat input = imread(fname, 1);
 
         if (!input.data) {
@@ -119,18 +120,74 @@ int main(int argc, char** argv )
         }
     }
 
-    /* run the given algorithm */
-    try {
-        auto interface = algorithms.at(args["<algorithm>"].asString());
-        interface->parse_arguments(args, args["<args>"].asStringList());
-        /* pass input and arguments to each algorithm 
-         * in case of camera or video input; do this indefinitely */ 
-         // TODO
-        interface->run_algorithm(images, image_names, video_frames, cameras);
-    } catch (std::out_of_range e) {
-        std::cout << "Algorithm " << args["<algorithm>"]
-                  << " not found" << std::endl;
+    /* parse parameters for each algorithm */
+    std::vector<std::shared_ptr<FrameAlgorithm>> active_algorithms;
+    std::vector<std::string> active_names;
+    while (true) {
+        try {
+            if (args["<algorithm>"].isEmpty()) {
+                break;
+            }
+            std::string name = args["<algorithm>"].asString();
+            active_names.push_back(name);
+            active_algorithms.push_back(algs.at(name)());
+        } catch (std::out_of_range e) {
+            std::cout << "Algorithm " << args["<algorithm>"] << " not found: "
+                      << std::string(e.what()) << std::endl;
+        }
+        args = active_algorithms.back()->
+            parse_arguments(main_args, args["<args>"].asStringList());
     }
+
+    std::cout << "Press q or ESC, or type Ctrl-C to exit." << std::endl;
+
+    /* run each algorithm in sequence on every image */
+    std::vector<cv::Mat> outputs(images.size());
+    for (int i = 0; i < active_algorithms.size(); i++) {
+            for (int j = 0; j < images.size(); j++) {
+                auto prefix = image_names[j] + active_names[i];
+                active_algorithms[i]->
+                    process_frame(images[j], outputs[j], prefix);
+                save_mat(prefix, outputs[j], true, true);
+            }
+        images = outputs;
+    }
+
+    /* main loop: read from available cameras and show OpenCV windows */
+    if (!main_args["--no-gui"].asBool()) {
+        std::vector<cv::Mat> outputs(frames.size());
+        long frame_number = 0;
+        while (true) {
+            /* run each algorithm on each frame */
+            for (int i = 0; i < frames.size(); i++) {
+                save_mat("input_" + std::to_string(i), frames[i], true, true);
+                for (int j = 0; j < active_algorithms.size(); j++) {
+                    auto prefix = active_names[j] + "_" + std::to_string(i);
+                    active_algorithms[j]->
+                        process_frame(frames[i], outputs[i], prefix);
+                    save_mat(prefix, outputs[i], true, true);
+
+                    if (j < active_algorithms.size() - 1) {
+                        frames[i] = outputs[i];
+                    } else {
+                        outputs[i] = Mat();
+                    }
+                }
+            }
+
+            /* get new frames */
+            for (int i = 0; i < cameras.size(); i++) {
+                cameras[i] >> frames[i];
+            }
+
+            auto key = cv::waitKey(10);
+            if (key == 'q' || key == 'Q' || key == 27) {
+                break;
+            }
+            frame_number++;
+        }
+    }
+
 
     return 0;
 }
--- src/util.hpp
+++ src/util.hpp
@@ -7,114 +7,66 @@
 #include "../lib/docopt.cpp/docopt.h"
 
 inline void save_mat(const std::string name, const cv::Mat mat,
-                     bool save, bool gui) {
+                     bool save, bool gui)
+{
     if (save) {
         if (gui) {
             cv::namedWindow(name, cv::WINDOW_NORMAL);
             cv::imshow(name, mat);
         } else {
-            cv::Mat _mat;
+            cv::Mat _mat(mat);
             if (mat.type() != CV_8UC3) {
                 mat.convertTo(_mat, CV_8UC3, 256);
             }
-            std::string fname = "_" + name + ".png";
-            std::cout << "writing to '" << fname << "'" << std::endl;
-            cv::imwrite(fname, _mat);
+            std::cout << "Writing to '_" << name << ".png'" << std::endl;
+            cv::imwrite("_" + name + ".png", _mat);
         }
-    };
+    }
 }
 
 inline float docopt_to_float(std::map<std::string, docopt::value> args,
-                             std::string key, float default_v,
-                             bool print_message=true)
+                             std::string key, float default_v)
 {
     if (!args[key].isEmpty()) {
         try {
             return std::stof(args[key].asString());
         } catch (std::invalid_argument) {
-            if (print_message) {
-                std::cout << "Argument '" << key << "' expected number, got "
-                          << args[key] << " defaulting to " << default_v << std::endl;
-            }
+            std::cout << "Argument '" << key << "' expected number, got "
+                      << args[key] << " using " << default_v << std::endl;
         }
     }
     return default_v;
 }
 
-class Interface {
+/* this class controls the setup and activity of an algorithm that takes a
+ * single input and has a main output
+ *
+ * docopt is used to control algorithm parameters
+ *
+ * by default,
+ * run_on_images calls process_mat on each image
+ * frames are read from every camera and
+ * run_on_frames calls process_mat on each frame
+ */
+class FrameAlgorithm {
 public:
-    Interface() {}
-    Interface(int _output_format, std::string _doc) :
-        output_format(_output_format), doc(_doc) {}
+    FrameAlgorithm(std::string _doc) : doc(_doc) {}
+
     std::map<std::string, docopt::value> args;
     std::map<std::string, docopt::value> main_args;
-    std::string doc = "";
-    int output_format = 0;
 
-    virtual bool check_inputs(int n_images, int n_cameras)
-    {
-        if (n_images + n_cameras == 0) {
-            std::cout << "one or more inputs expected" << std::endl;
-            return false;
-        }
-        return true;
-    }
+    std::string doc = "Usage: algorithm [<algorithm> [<args>...]]";
+
     virtual void process_frame(const cv::Mat& in, cv::Mat& out,
                                std::string prefix="") = 0;
 
-    inline virtual void parse_arguments(std::map<std::string, docopt::value>
-                                        main_args, std::vector<std::string>
-                                        argv)
+    inline virtual std::map<std::string, docopt::value>
+    parse_arguments(std::map<std::string, docopt::value> main_args,
+                    std::vector<std::string> argv)
     {
         this->main_args = main_args;
-        this->args = docopt::docopt(this->doc, argv, true);
-    }
-
-    /* by default, calls process_frame on each image
-     * and reads new frames from each camera at least every 10ms
-     * then runs process_frame on each
-     */
-    inline virtual void run_algorithm(std::vector<cv::Mat>& images,
-                       std::vector<std::string>& image_names,
-                       std::vector<cv::Mat>& frames,
-                       std::vector<cv::VideoCapture>& cameras)
-    {
-        if (!check_inputs(images.size(), cameras.size())) {
-            return;
-        }
-        for (int i = 0; i < images.size(); i++) {
-            cv::Mat output (images[i].size(), this->output_format);
-            this->process_frame(images[i], output, image_names[i]);
-            save_mat("in_" + image_names[i], images[i], true,
-                     !main_args["--no-gui"].asBool());
-            save_mat("output_" + image_names[i], output, true,
-                     !main_args["--no-gui"].asBool());
-        }
-        std::vector<cv::Mat> outputs (frames.size());
-        bool first = true;
-        while (true) {
-            if (cameras.size() > 0 && !main_args["--no-gui"].asBool()) {
-                for (int i = 0; i < cameras.size(); i++) {
-                    cameras[i] >> frames[i];
-                    if (first) {
-                        outputs[i] = cv::Mat(frames[i].size(),
-                                             this->output_format);
-                        std::cout << "Press Q or ESC or type Ctrl-C to exit."
-                                  << std::endl;
-                    }
-                    this->process_frame(frames[i], outputs[i], "camera_");
-                    save_mat("camera_" + i, outputs[i], true,
-                             !main_args["--no-gui"].asBool());
-                    save_mat("out_camera_" + i, frames[i], true,
-                             !main_args["--no-gui"].asBool());
-                }
-            }
-            auto key = cv::waitKey(10);
-            if (key == 'q' || key == 'Q' || key == 27) {
-                break;
-            }
-            first = false;
-        }
+        this->args = docopt::docopt(this->doc, argv, true, "", true);
+        return {{"<algorithm>", docopt::value()}};
     }
 };
 
